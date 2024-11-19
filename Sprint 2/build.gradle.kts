95849b7ba28b3396436c2ca3092609821020adb5
Viictoor <117302343+victorrosalejo@users.noreply.github.com>
2024-11-19 14:50:57 +0100

    Sprint 2

---
+++ Sprint 2/build.gradle.kts
@@ -0,0 +1,9 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.gms:google-services:4.3.15")
+    }
+}
---
+++ Sprint 2/settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Sprint2-GoBook"
+include(":app")
